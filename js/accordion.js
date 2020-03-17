01e62b8c5262170d604bfcb740cabe198f638c80
Hiren <hiren@hi-labsolution.com>
2020-03-17 11:45:03 +0530

    initial commit

---
+++ js/accordion.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	// Accordian JS
+	$(".accordion-heading").click(function(e){
+		e.preventDefault();
+		if ($(".accordion-body").is(":visible")) {
+			$(".accordion-body").slideUp(700);
+			$(".accordion-heading").removeClass("active");
+		}
+    if( $(this).next(".accordion-body").is(":visible")){
+        $(this).next(".accordion-body").slideUp(700);
+        $(".accordion-heading").removeClass("active");
+    }	else {
+        $(this).next(".accordion-body").slideDown(700); 
+       	$(this).addClass("active");
+    }
+	});
+});
